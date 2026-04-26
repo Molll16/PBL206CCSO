3371fa7d200fdb5ef8665fdd7ae852fe29b43917
BintangDwiImamDermawan <bintangdwi190@gmail.com>
2026-04-26 16:37:32 +0700

    update seeder buat generate fake acc, call fitur

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
+            'no_telp' => fake()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
